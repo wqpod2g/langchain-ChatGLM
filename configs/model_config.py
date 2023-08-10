73b8d19cdc85b5bee176f789c66766be7a161c73
Qiang Wang <903183867@qq.com>
2023-08-10 15:00:33 +0800

    Update model_config.py
    update

--- configs/model_config.py
+++ configs/model_config.py
@@ -228,7 +228,7 @@ llm_model_dict = {
 }
 
 # LLM 名称
-LLM_MODEL = "chatglm2-6b-32k"
+LLM_MODEL = "chatglm2-6b"
 # 量化加载8bit 模型
 LOAD_IN_8BIT = False
 # Load the model with bfloat16 precision. Requires NVIDIA Ampere GPU.
